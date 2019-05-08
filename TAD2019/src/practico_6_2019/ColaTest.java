f9f3ef0a0ad873eeb87c7fd729a7333eb601a4f9
FERNANDO <FERNANDO@FERNANDO-PC>
2019-05-08 17:44:20 -0300

    ultima

---
+++ TAD2019/src/practico_6_2019/ColaTest.java
@@ -0,0 +1,26 @@
+
+package practico_6_2019;
+import colaDinamica.*;
+import recursos.*;
+import pilaDinamica.pilaDinamica;
+
+public class ColaTest extends Cola {
+    
+    //obtener el cardinal de una cola
+    public int cardinal(Cola cola){
+        int c = 0;
+        Nodo aux = cola.inicio;
+        while(aux != cola.fin){
+            c++;
+        }
+            return c;
+    }
+            
+    //invertir una cola
+    public void invertirCola(Cola cola){
+        pilaDinamica pila = new pilaDinamica();
+        Nodo aux = pila.
+        while()
+    }
+    
+}
