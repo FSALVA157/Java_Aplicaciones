af47d148cb7b129212f55cc86d17501401e79024
Fernando Javier Salva <xxavierargentino@hotmail.com>
2019-05-08 22:26:31 -0300

    ejercicio 8 practico 6

--- TAD2019/src/colaDinamica/Cola.java
+++ TAD2019/src/colaDinamica/Cola.java
@@ -3,8 +3,9 @@ package colaDinamica;
 
 import recursos.*;
 public class Cola implements IContenedor{
-   public Nodo inicio;
-   public Nodo fin;
+   Nodo inicio;
+   Nodo fin;
+   int contador;
    /* 
     public Cola(Object elemento){
         inicio = fin = new Nodo(elemento);
@@ -22,7 +23,7 @@ public class Cola implements IContenedor{
             this.fin.setSiguiente(new Nodo(elemento));
             this.fin = fin.getSiguiente();
         }
-        
+        this.contador++;
     }
 
     @Override
@@ -31,10 +32,13 @@ public class Cola implements IContenedor{
       if(!this.estaVacia()){
             auxiliar = this.inicio.getDato();
             inicio = inicio.getSiguiente();
+            this.contador--;
             //pequeña porcion del codigo que verifica si la cola no tenia un solo elemento
             if(this.estaVacia()){
                 this.fin = null;
+                contador = -1;
             }
+            
       }else{
           System.out.println("ERROR AL SACAR: LA COLA ESTA VACIA");
       }
@@ -44,11 +48,32 @@ public class Cola implements IContenedor{
     @Override
     public void limpiar() {
         inicio = fin = null;
+        this.contador = -1;
     }
 
     @Override
     public boolean estaVacia() {
         return(this.inicio == null);
-    }
+       }
     
+    public int cardinal(){
+        int aux = 0;
+        if(!this.estaVacia()){
+            aux = this.contador + 1;
+        }
+        return aux;
+     }
+    
+    //esta clase deberia ser abstracta pero como espero testear solo con tipos primitivos la implemento aqui
+    public void recorrer(){
+        if(!this.estaVacia()){
+        Nodo auxiliar = this.inicio;
+        while(auxiliar != null){
+            System.out.print("[" + auxiliar.getDato().toString() + "]--->");
+            auxiliar = auxiliar.getSiguiente();
+        }
+    }else{
+            System.out.println("NADA QUE MOSTRAR: LA COLA ESTA VACIA");
+        }
+    }
 }
--- TAD2019/src/practico_6_2019/ColaTest.java
+++ TAD2019/src/practico_6_2019/ColaTest.java
@@ -1,26 +1,53 @@
 
 package practico_6_2019;
 import colaDinamica.*;
+import java.security.SecureRandom;
+import jdk.nashorn.internal.codegen.CompilerConstants;
 import recursos.*;
 import pilaDinamica.pilaDinamica;
 
 public class ColaTest extends Cola {
     
     //obtener el cardinal de una cola
-    public int cardinal(Cola cola){
-        int c = 0;
-        Nodo aux = cola.inicio;
-        while(aux != cola.fin){
-            c++;
-        }
-            return c;
+    public static int cardinal(Cola cola){
+        return cola.cardinal();
     }
             
     //invertir una cola
-    public void invertirCola(Cola cola){
+    public static void invertirCola(Cola cola){
+        
+        System.out.println("-----COLA ORIGINAL--------");
+       cola.recorrer();
         pilaDinamica pila = new pilaDinamica();
-        Nodo aux = pila.
-        while()
+        //vuelco la cola en una pila auxiliar
+        while(!cola.estaVacia()){
+            pila.meter(cola.sacar());
+        }
+        System.out.println("");
+        //ahora vuelvo a formar una cola a partir de la pila auxiliar
+        while(!pila.estaVacia()){
+            cola.meter(pila.sacar());
+        }
+        System.out.println("-----COLA INVERTIDA--------");
+        cola.recorrer();
+    }
+    
+    public static void main(String Args[]){
+         //voy a llenar una cola de forma aleatoria 
+         SecureRandom ran = new SecureRandom();
+         Cola stack = new Cola();
+         for(int i = 0; i < 7;i++){
+             int valor = 1 + ran.nextInt(99);
+             stack.meter(valor);
+          }
+         System.out.println("el tamaño de la cola es: " + cardinal(stack));
+         stack.recorrer();
+         invertirCola(stack);
+         
+         
+        
+        
+        
     }
     
 }
