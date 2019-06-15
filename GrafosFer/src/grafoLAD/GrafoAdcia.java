2a5e284b3e4dd4c99aa17d8d7b78f0faeb52dd3f
xxavierargentino <33401263+xxavierargentino@users.noreply.github.com>
2019-06-15 13:05:43 -0300

    listas de Adyacencia en un grafo_Inicio

---
+++ GrafosFer/src/grafoLAD/GrafoAdcia.java
@@ -0,0 +1,25 @@
+
+package grafoLAD;
+
+//voy a implementar la lista de vertices con una lista doble 
+
+import grafoDirigido.ListaDoble;
+import grafoNoDirigido.ConjuntoAbs;
+
+public class GrafoAdcia extends ConjuntoAbs{
+    int tamanio;
+    VerticeAdcia vertice;
+
+    @Override
+    public boolean esIgual(Object obj1, Object obj2) {
+        
+    }
+
+    @Override
+    public String imprime(Object obj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
---
+++ GrafosFer/src/grafoLAD/ListaAdcia.java
@@ -0,0 +1,22 @@
+
+package grafoLAD;
+
+import grafoNoDirigido.ConjuntoAbs;
+
+
+public class ListaAdcia extends ConjuntoAbs {
+private Arco arco;
+
+  
+
+    @Override
+    public boolean esIgual(Object obj1, Object obj2) {
+     return ((Arco)obj1).getDestino() == ((Arco)obj2).getDestino();
+    }
+
+    @Override
+    public String imprime(Object obj) {
+        return "[destino: " + ((Arco)(obj)).getDestino() + "| peso: " + ((Arco)(obj)).getPeso();
+    }
+
+}
