ff3114123806918a12fd691b1335b0d178c4daf5
Fernando Javier Salva <xxavierargentino@hotmail.com>
2019-06-20 23:28:37 -0300

    actualizacionProyecto

---
+++ GrafosFer/src/grafoLAD/AbsGrafoAdciaD.java
@@ -0,0 +1,14 @@
+
+package grafoLAD;
+
+
+public abstract class AbsGrafoAdciaD implements IOperacionesG {
+    protected ListaVerticesAdcia matrizDinGrafo;
+    
+    public AbsGrafoAdciaD(){
+        //como resolver este problema???
+        matrizDinGrafo = new ListaVerticesAdcia();
+        }
+    }
+    
+}
